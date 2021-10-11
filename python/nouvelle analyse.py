b8e07872c46dfff427df09809a224d0e285704c6
TiibCD <tiib5@hotmail.com>
2021-10-11 16:55:07 +0200

    Ajout gestion type analyse

--- python/nouvelle analyse.py
+++ python/nouvelle analyse.py
@@ -1,27 +1,36 @@
 import os
 import shutil
 
+type_analyse = input("EDD (0) / WS (1) : ")
 nom_client = input("Nom client : ")
 nom_fichier = nom_client + '.txt'
 dossier_courant = os.getcwd()
 dossier_test = 'c:/test'
-fichier_modele = dossier_courant + '/_Analyse.txt'
+analyse_modele_EDD = dossier_courant + '/_Analyse.txt'
+analyse_modele_WS = dossier_courant + '/WS/_Support WS.txt'
 
+# Analyse EDD
 # On crée le dossier client qui contiendra les fichiers fournis par le client
-try:
-	os.mkdir(dossier_courant + '/fichiers des clients/' + nom_client)
-except OSError as error:
-	print(error)
-
+if type_analyse == '0':
+    try:
+        os.mkdir(dossier_courant + '/fichiers des clients/' + nom_client)
+    except OSError as error:
+        print(error)
 # On crée un fichier d'analyse pour le client en partant du fichier modèle
-fichier_new = dossier_courant + '/notes/' + nom_fichier
-shutil.copy(fichier_modele, fichier_new)
+    fichier_new = dossier_courant + '/notes/' + nom_fichier
+    shutil.copy(analyse_modele_EDD, fichier_new)
 
 # On crée le dossier contenant les fichiers de test de l'interface du client
 # Ajouter une possibilité de désactiver ca ?
-try:
-    os.mkdir(dossier_test + '/' + nom_client)
-except OSError as error:
-    print(error)
+    try:
+        os.mkdir(dossier_test + '/' + nom_client)
+    except OSError as error:
+        print(error)
+
+# Analyse WS
+else:
+    fichier_new = dossier_courant + '/WS/' + nom_fichier
+    shutil.copy(analyse_modele_WS, fichier_new)
+
 
 # TODO On ouvre le fichier généré dans vs code
