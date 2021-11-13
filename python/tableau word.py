8901a9f4a33ad1a646fd3204b42e13298168392d
TiibCD <tiib5@hotmail.com>
2021-11-13 12:31:09 +0100

    ajout gestion fichier json

--- python/tableau word.py
+++ python/tableau word.py
@@ -1,40 +1,43 @@
-import operator
+import json
 from docx import Document
 # import subprocess
 
 
-# def sort_table(tableau, col):
-#    return sorted(tableau, key=operator.itemgetter(col))
+# Ouverture du fichier json
+with open('template.json', 'r', encoding='UTF-8') as json_data:
+    enregistrements = json.load(json_data)
 
+# Tri des élements par position
+enregistrements = sorted(
+    enregistrements, key=lambda entry: int(entry['position']))
 
+# Création du document et du tableau
 document = Document()
-
-records = (
-    #('Notion', 'Position', 'Titre', 'Longueur'),
-    ('données perso', '3', 'Nom', '24'),
-    ('données perso', '2', 'Prénom', '24'),
-    ('données perso', '1', 'Matricule', '13'),
-    ('données perso', '0', 'Civilité', '4'),
-)
-
-records = sorted(records, key=lambda entry: entry[1])
-# records = sort_table(records, 1)
-
-
-tableau = document.add_table(rows=1, cols=4)
-hdr_cells = tableau.rows[0].cells
+tableau_word = document.add_table(rows=1, cols=6)
+hdr_cells = tableau_word.rows[0].cells
 hdr_cells[0].text = 'Notion'
 hdr_cells[1].text = 'Position'
 hdr_cells[2].text = 'Titre'
-hdr_cells[3].text = 'Longueur'
+hdr_cells[3].text = 'Format'
+hdr_cells[4].text = 'Exemple'
+hdr_cells[5].text = 'Commentaire'
 
-
-for notion, position, titre, longueur in records:
-    if position != '0':
-        row_cells = tableau.add_row().cells
-        row_cells[0].text = notion
-        row_cells[1].text = position
-        row_cells[2].text = titre
-        row_cells[3].text = longueur
+# Ecriture des élements dans le tableau (si position !=0)
+for entity in enregistrements:
+    if entity['position'] != '0':
+        row_cells = tableau_word.add_row().cells
+        row_cells[0].text = entity['notion']
+        row_cells[1].text = entity['position']
+        row_cells[2].text = entity['champ']
+        row_cells[3].text = entity['format']
+        row_cells[4].text = entity['exemple']
+        row_cells[5].text = entity['commentaire']
 
 document.save('test.docx')
+
+# RAZ des positions dans le fichier json
+for entity in enregistrements:
+    entity['position'] = '0'
+# Ecriture du fichier json
+with open('template.json', 'w', encoding='UTF-8') as json_data:
+    json.dump(enregistrements, json_data, indent=2)
